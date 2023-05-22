7db172382896c422cfed6561453104d7f08a6871
Walef <walefsouza05@gmail.com>
2023-05-22 15:59:39 -0300

    Projeto finalizado

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Projeto_simples</title>
+</head>
+
+<body>
+    <div class="container">
+
+        <!-- cabeçalho da pagina-->
+        <div class="cabecalho">
+            <h1 class="titulo">Nvidia</h1>
+            <p class="paragrafo1">NVIDIA ANUNCIA LANÇAMENTO DAS PLACAS DE VÍDEO GEFORCE RTX 4060 COM AVANÇOS TECNOLÓGICOS E SUPORTE AO DLSS</p>
+        </div>
+        <!-- Imagem de destaque-->
+        <div class="img-destaque">
+            <figure>
+                <img src="img/nvidia2.jpeg" alt="destaque" class="img">
+                <figcaption class="legenda">Nvidia Corporation estilizada como NVIDIA ou, devido ao seu logotipo, nVIDIA</figcaption>
+            </figure>
+        </div>
+        <!-- Historia/ biografia da empresa-->
+        <div class="texto">
+            <h2>Quem é Nvidia ?</h2>
+            <p >
+                Nvidia Corporation estilizada como NVIDIA ou, devido ao seu logotipo,
+                nVIDIA é uma empresa multinacional de tecnologia incorporada em Delaware e com sede em Santa Clara,
+                Califórnia.
+                Ela projeta unidades de processamentos gráficos conhecidas como (GPUs) para os mercados de jogos eletrônicos e profissionais,
+                bem como o sistema em unidades de chip (SoCs) para o mercado de computação móvel e automotivo.
+                Sua linha principal de produtos GPU, denominada "GeForce", está em concorrência direta com os produtos "Radeon" da Advanced Micro Devices (AMD).
+                <span class="destaque-texto"> Nvidia expandiu sua presença na indústria de jogos com seus portáteis Shield Portable, Shield Tablet e Shield Android TV.
+                    Desde de 2014, a Nvidia mudou para se tornar uma empresa de plataforma focada em cinco mercados – jogos, visualização profissional,
+                    data centers , auto e o mercado de criptomoedas.</span> Nvidia também está agora concentrada na inteligência artificial.
+            </p>
+        </div>
+        <!-- Primeira citação -->
+        <div class="frase-container">
+            <blockquote>"The way it's meant to be played." (em português: "Da maneira que é suposto ser jogado.")</blockquote>
+            <p> - Nvidia.</p>
+        </div>
+        <!-- Curiosidades -->
+        <div class="curiosidades">
+            <h2>Curiosidades sobre a Nvidia</h2>
+            <ul>
+                <li>A Nvidia QUASE foi à falência </li>
+                <li>GeForce 256 e a invenção da GPU</li>
+                <li>Nvidia GTC e o investimento em inteligência artificial</li>
+                <li>Investimento na área da saúde</li>
+                <li>Primeiro Ultrassom 3D da história</li>
+            </ul>
+        </div>
+          <!-- segunda citação -->
+          <div class="frase-container">
+            <blockquote>CEO da Nvidia está entre as <span class="destaque-texto">100 pessoas mais influentes </span> 2021</blockquote>
+            <p> - Time.</p>
+        </div>
+        <!-- Segunda imagem -->
+        <div class="img-destaque">
+            <figure>
+                <img src="img/nvidia3.jpeg" alt="destaque" class="img">
+                <figcaption class="legenda">Nvidia Corporation estilizada como NVIDIA ou, devido ao seu logotipo, nVIDIA</figcaption>
+            </figure>
+        </div>
+        <!-- Biografia/referencias/links -->
+        <div class="biografia-links">
+            <h2>Links</h2>
+            <p>você pode conhecer mais acessando o <a href="https://www.nvidia.com/pt-br/">link.</a></p>
+
+        </div>
+    </div>
+</body>
+
+</html>
